ab2c2117baa5050a88f1d060b6ee178c2cb076c1
Arif <phys.arif@gmail.com>
2026-07-24 04:09:52 +0600

    Update generate_pdf.py

--- scripts/generate_pdf.py
+++ scripts/generate_pdf.py
@@ -122,7 +122,7 @@ def pdf_chapters_page_css(title):
     safe_title = _css_escape(title)
     return (
         "@page {"
-        "  size: 100mm 144.7mm;"
+        "  size: 110mm 210mm;"
         "  margin: 10mm 8mm 18mm 8mm;"
         "  @bottom-left {"
         f'    content: "{safe_title}";'
